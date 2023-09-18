9e37fa722d301094473bc3b31419ae39752abb6f
efeaizesogie <efeaizesogie@gmail.com>
2023-09-18 22:09:43 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LogoComponent } from './logo/logo.component';
+import { ButtonComponent } from './button/button.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LogoComponent,
+    ButtonComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
