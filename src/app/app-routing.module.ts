31ab6d2cc6333be559ca8b87aa286d93dfc73a9d
efeaizesogie <efeaizesogie@gmail.com>
2023-09-20 20:07:30 +0100

    edited routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "contact-us",
+    component: ContactPageComponent,
+  },
+  {
+    path: "",
+    component: LandingPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
